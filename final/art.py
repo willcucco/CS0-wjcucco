b8f12bb336061d309cee4dfd7adb57a003182c98
willcucco <willcucco@gmail.com>
2023-12-07 18:54:52 -0700

    Hangman cloud backup NOT FINAL

---
+++ final/art.py
@@ -0,0 +1,74 @@
+def display_hangman(tries):
+    """Displays the hangman drawing based on the number of tries left"""
+    ASCII = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return ASCII[tries]
--- final/hangman.py
+++ final/hangman.py
@@ -15,47 +15,170 @@ Steps:
 
 
 import random
+import art
+from rich.console import Console
+console = Console()
 
-def choose_word():
-    """Chooses a word at random and prints the amount of underscores equal to the amount of
-       letters in the chosen word
-    """
-    words = open("words.txt", 'r', encoding="utf-8")
-    words = words.readlines() # reads the words from the file
 
-    word = random.choice(words) # chooses a random word from the file
-    word = word.upper().strip() # makes word uppercase and strip trailing whitespaces
-
-    return word
+with open("words.txt", 'r', encoding="utf-8") as file: # opens words.txt for the program to read
+    list_of_words = file.readlines() # reads the file line by line
 
 
 def main():
-    """main function that solves the problem
+    """main function that initializes the game"""
+    word = get_word() # stores randomly chosen word to "word" variable
+    play_game(word) # goes to the play_game function with the "word" variable
+    while input("Would You Like To Play Again? (Yes/No) ").upper() == "YES": # if the user wants to play again
+        word = get_word()
+        play_game(word)
+
+
+def get_word():
+    """chooses a word at random
+
+    Returns:
+        str: the randomly chosen word
     """
-    word = choose_word()
-    guess = '_'*len(word) # prints '_' for how many letters are in the word
-    print(guess)
-    for i in range(len(word)+5):
-        letter = user_guess(word, guess)
-
-    #if letter == True:
-    #    ()
+    word = random.choice(list_of_words) # chooses a random word from the file
+    uppercase_word = word.upper() # makes word uppercase
+    return uppercase_word
 
 
-def user_guess(word, guess):
-    letter = input('Enter a guess: ')
-    found = False
-    for i in range(len(word)+5):
-        if word[i] == letter:
-            found = True
-            guess[i] = letter
+def play_game(word):
+    """main game logic
 
-    if found == False:
-        return False
-    if found == True:
-        return True
+    Args:
+        word (str): randomly chosen word
+    """
+    word_completion = "_" * (len(word)-1)
+    guessed = False
+    guessed_letters = []
+    tries = 6
+    print("Let's play Hangman!")
+    console.print(art.display_hangman(tries), style='bold')
+    console.print(word_completion, style="green")
+    print("\n")
+    if input("Would you like emojis? (Yes/No): ").upper() == "YES":
+        if input("Choose the color theme (Matrix/Normal): ").upper() == "MATRIX": # if the user chooses the normal color theme
+            while not guessed and tries > 0: # keeps the game going until the user runs out of tries
+                guess = input("Guess a letter: ").upper()
+                if len(guess) == 1 and guess.isalpha(): # it took me a while to realize that I needed this bit in line 64
+                    # because I realized that I was able to guess multiple letters, and it was confusing the program
+                    if guess in guessed_letters: # checks if the letter has already been guessed
+                        console.print("You've already guessed", guess + " :confused_face:", style="green")
+                    elif guess not in word:
+                        console.print(guess, "is not correct. :thumbs_down:", style="green")
+                        tries -= 1
+                        guessed_letters.append(guess)
+                    else:
+                        console.print("Good job,", guess, "is in the word! :thumbs_up:", style="green")
+                        guessed_letters.append(guess)
+                        word_as_list = list(word_completion)
+                        indices = [i for i, letter in enumerate(word) if letter == guess]  # Pylint suggested that I use enumerate
+                        # instead of iterating with range and len. I needed help with enumerate, so I went to youtube for a small part of these next few lines
+                        for index in indices: # checks if the guessed letter is in the word
+                            word_as_list[index] = guess
+                        word_completion = "".join(word_as_list)
+                        if "_" not in word_completion:
+                            guessed = True
+                else: # if the user enters something other than a letter
+                    console.print("Not a valid guess. Try again. :confused_face:", style="green")
+                console.print(art.display_hangman(tries), style='bold')
+                console.print(word_completion, style="green")
+                print("\n")
+            if guessed is True: # if the user guessed the word
+                console.print("Congrats, you guessed the word! :smile:", style="green")
+            else: # if the user ran out of tries
+                console.print("GAME OVER. The word was " + word.strip() + ". :disappointed_face:", style="green")
+        else: # if the user chooses the normal color theme
+            while not guessed and tries > 0: # keeps the game going until the user runs out of tries
+                guess = input("Guess a letter: ").upper()
+                if len(guess) == 1 and guess.isalpha():
+                    if guess in guessed_letters: # checks if the letter has already been guessed
+                        console.print("You've already guessed", guess + " :confused_face:", style="yellow")
+                    elif guess not in word: # if the user guesses a letter that is not in the word
+                        console.print(guess, "is not correct. :thumbs_down:", style="red")
+                        tries -= 1
+                        guessed_letters.append(guess)
+                    else:
+                        console.print("Good job,", guess, "is in the word! :thumbs_up:", style="green")
+                        guessed_letters.append(guess)
+                        word_as_list = list(word_completion)
+                        indices = [i for i, letter in enumerate(word) if letter == guess]
+                        for index in indices: # checks if the guessed letter is in the word
+                            word_as_list[index] = guess
+                        word_completion = "".join(word_as_list)
+                        if "_" not in word_completion:
+                            guessed = True
+                else:
+                    console.print("Not a valid guess. Try again. :confused_face:", style="red")
+                console.print(art.display_hangman(tries), style='bold')
+                console.print(word_completion, style="blue")
+                print("\n")
+            if guessed is True: # if the user guessed the word
+                console.print("Congrats, you guessed the word! :smile:", style="green")
+            else: # if the user ran out of tries
+                console.print("GAME OVER. The word was " + word.strip() + ". :disappointed_face:", style="red")
+    else: # if the user does not want emojis
+        if input("Choose the color theme (Matrix/Normal): ").upper() == "MATRIX": # if the user wants the Matrix color theme
+            while not guessed and tries > 0: # keeps the game going until the user runs out of tries
+                guess = input("Guess a letter: ").upper()
+                if len(guess) == 1 and guess.isalpha():
+                    if guess in guessed_letters: # checks if the letter has already been guessed
+                        console.print("You've already guessed", guess, style="green")
+                    elif guess not in word: # if the user guesses a letter that is not in the word
+                        console.print(guess, "is not correct.", style="green")
+                        tries -= 1
+                        guessed_letters.append(guess)
+                    else: # if the user guesses a letter that is in the word
+                        console.print("Good job,", guess, "is in the word!", style="green")
+                        guessed_letters.append(guess)
+                        word_as_list = list(word_completion)
+                        indices = [i for i, letter in enumerate(word) if letter == guess]
+                        for index in indices:
+                            word_as_list[index] = guess
+                        word_completion = "".join(word_as_list)
+                        if "_" not in word_completion:
+                            guessed = True
+                else: # if the user enters something other than a letter
+                    console.print("Not a valid guess. Try again.", style="green")
+                console.print(art.display_hangman(tries), style='bold')
+                console.print(word_completion, style="green")
+                print("\n")
+            if guessed is True: # if the user guessed the word
+                console.print("Congrats, you guessed the word!", style="green")
+            else: # if the user ran out of tries
+                console.print("GAME OVER. The word was " + word.strip() + ".", style="green")
+        else: # if the user chooses normal color theme
+            while not guessed and tries > 0: # keeps the game going until the user runs out of tries
+                guess = input("Guess a letter: ").upper()
+                if len(guess) == 1 and guess.isalpha():
+                    if guess in guessed_letters: # checks if the letter has already been guessed
+                        console.print("You've already guessed", guess, style="yellow")
+                    elif guess not in word: # if the user guesses a letter that is not in the word
+                        console.print(guess, "is not correct.", style="red")
+                        tries -= 1
+                        guessed_letters.append(guess)
+                    else: # if the user guesses a letter that is in the word
+                        console.print("Good job,", guess, "is in the word!", style="green")
+                        guessed_letters.append(guess)
+                        word_as_list = list(word_completion)
+                        indices = [i for i, letter in enumerate(word) if letter == guess]
+                        for index in indices: # checks if the guessed letter is in the word
+                            word_as_list[index] = guess
+                        word_completion = "".join(word_as_list)
+                        if "_" not in word_completion:
+                            guessed = True
+                else: # if the user enters something other than a letter
+                    console.print("Not a valid guess. Try again.", style="red")
+                console.print(art.display_hangman(tries), style='bold')
+                console.print(word_completion, style="blue")
+                print("\n")
+            if guessed is True: # if the user guessed the word
+                console.print("Congrats, you guessed the word!", style="green")
+            else: # if the user ran out of tries
+                console.print("GAME OVER. The word was " + word.strip() + ".", style="red")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
